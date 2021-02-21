3dc3ca18f429141dbabc4bbc9cf08b1ab19dcc05
JuanHasbunZem <zemenarwow@gmail.com>
2021-02-21 10:44:33 -0800

    basic outline of plan
    Co-authored-by: Collin Chapman <cchap14@gmail.com>
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-function diceRoll() {
-  min = Math.ceil(1);
-  max = Math.floor(6);
+//Business Logic Stuff
+function diceRoll(min, max) {
+  let min = Math.ceil(min);
+  let max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function playerOne
 
 function playerTwo
+
+
+// UI Logic Stuff
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    const playerOneName = $("input#playerOneName").val();
+    const playerTwoName = $("input#playerTwoName").val();
+
+  });
