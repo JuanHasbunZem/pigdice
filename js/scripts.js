e7350348bfed95060b5182f96092d54b5b894066
JuanHasbunZem <zemenarwow@gmail.com>
2021-02-24 17:26:40 -0800

    add gamersList constructor and id functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 //Business Logic Stuff
+function ListOfGamers() {
+  this.gamers = {};
+  this.currentId = 0;
+}
+
+ListOfGamers.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+ListOfGamers.prototype.addGamer = function(gamer) {
+  gamer.id = this.assignId();
+  this.gamers[gamer.id] = gamer;
+}
+
+ListOfGamers.prototype.findGamer = function(id) {
+  if (this.gamers[id] != undefined) {
+    return this.gamers[id];
+  }
+  return false;
+}
+
 function Gamer(name) {
   this.name = name;
   this.tempScore = 0;
@@ -10,26 +32,29 @@ function diceRoll() {
 }
 
 Gamer.prototype.checkScore = function(score) {
-  if (Gamer.score >= 100) {
+  if (this.score >= 100) {
     $("#gamePlayOne").hide();
     $("#gamePlayTwo").hide();
     $("#playOneWin").show();
   } else {
-    Gamer.prototype.tempScore(score);
+    gamersList.gamers[1].tempScore(score);
   }
 }
 
 Gamer.prototype.tempScore = function(dice) {
-  this.tempScore = tempScore + dice;
+  this.tempScore = this.tempScore + dice;
+  return this.tempScore;
 }
 
-Gamer.prototype.score = function(scored) {
-  this.score = Gamer.tempScore;
+Gamer.prototype.score = function(score) {
+  this.score = this.score + this.tempScore;
+  return this.score;
 }
 
 
 // UI Logic Stuff
-//Starting Scores:
+let gamersList = new ListOfGamers();
+
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
@@ -39,39 +64,66 @@ $(document).ready(function() {
     $(".playerTwoName").text(playerTwoName);
     $("#formOne").hide();
     $("#gamePlayOne").show();
-    $("#scoreDisplay").show(); 
+    $("#scoreDisplay").show();
 
     let playerOne = new Gamer(playerOneName);
     let playerTwo = new Gamer(playerTwoName);
+    gamersList.addGamer(playerOne);
+    gamersList.addGamer(playerTwo);
   });
+  
 
   $("#rollDiceOne").click(function(event) {
     event.preventDefault();
-    let rolled = rollDice();
-    let scored = playerOne.score;
-    if (rolled = 1) {
-      playerOne.score = 0;
-      playerOne.tempScore = 0;
-      playerOne.prototype.score(0);
+    let rolled = diceRoll();
+    if (rolled === 1) {
+      this.gamers[1].tempScore = 0;
+      this.gamers[1].score(0);
     } else {
-      playerOne.prototype.tempScore(rolled);
-      playerOne.prototype.checkScore(scored);
+      Gamer.checkScore(rolled);
     }
+  });
+
+  $("#rollDiceTwo").click(function(event) {
+    event.preventDefault();
+    playerTwo();
   });
 
   $("#holdDiceOne").click(function(event) {
     event.preventDefault();
-    playerOne.prototype.score(playerOne.tempScore);
+    playerScoreOne();
+    checkScoreOne();
     $("#gamePlayOne").hide();
     $("#gamePlayTwo").show();
-    playerOne.tempScore = 0;
+    tempScore = 0;
   });
 
-    $("#giveUpOne").click(function(event) {
+  $("#holdDiceTwo").click(function(event) {
+    event.preventDefault();
+    playerScoreTwo();
+    checkScoreTwo();
+    $("#gamePlayOne").show();
+    $("#gamePlayTwo").hide();
+    tempScore = 0;
+  });
+
+
+  $("#giveUpOne").click(function(event) {
     event.preventDefault();
     $("#formOne").show();
     $("#gamePlayOne").hide();
     $("#gamePlayTwo").hide();
     $("#scoreDisplay").hide();
   });
+
+  $("#giveUpTwo").click(function(event) {
+    event.preventDefault();
+    $("#formOne").show();
+    $("#gamePlayOne").hide();
+    $("#gamePlayTwo").hide();
+    $("#scoreDisplay").hide();
+  });
+
+
+
 });
